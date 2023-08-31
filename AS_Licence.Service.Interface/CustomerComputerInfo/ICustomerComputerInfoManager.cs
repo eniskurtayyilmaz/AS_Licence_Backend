31d533b273bbad7de3fd996833595282836f7e4c
aligbayir <a.bayrgorkem@gmail.com>
2023-08-31 16:48:01 +0300

    customercomputerinfo controller'a delete endpoint eklendi

--- AS_Licence.Service.Interface/CustomerComputerInfo/ICustomerComputerInfoManager.cs
+++ AS_Licence.Service.Interface/CustomerComputerInfo/ICustomerComputerInfoManager.cs
@@ -9,21 +9,21 @@ using AS_Licence.Entities.Model.CustomerComputerInfo;
 
 namespace AS_Licence.Service.Interface.Customer
 {
-  public interface ICustomerComputerInfoManager
-  {
-    Task<OperationResponse<List<CustomerComputerInfo>>> GetCustomerComputerInfoList(
-      Expression<Func<CustomerComputerInfo, bool>> filter = null,
-      Func<IQueryable<CustomerComputerInfo>, IOrderedQueryable<CustomerComputerInfo>> orderBy = null,
-      string includeProperties = "");
-    Task<OperationResponse<CustomerComputerInfo>> SaveCustomerComputerInfo(CustomerComputerInfo customer);
-    Task<OperationResponse<CustomerComputerInfo>> DeleteCustomerComputerInfoByCustomerComputerInfoId(int id);
-    Task<OperationResponse<CustomerComputerInfo>> GetByCustomerComputerInfoId(int id);
-    Task<OperationResponse<CustomerComputerInfo>> UpdateCustomerComputerInfo(CustomerComputerInfo model);
-    Task<OperationResponse<List<CustomerComputerInfo>>> GetByCustomerComputerInfoListBySubscriptionId(
-      int SubscriptionId);
-    Task<OperationResponse<int>> GetAlreadyComputerCountsBySubscriptionId(int subscriptionId);
-    Task<OperationResponse<CustomerComputerInfo>> GetByCustomerComputerHddAndMacAndProcessSerialCode(int subscriptionId, string hddCode,
-      string macCode, string processCode);
+    public interface ICustomerComputerInfoManager
+    {
+        Task<OperationResponse<List<CustomerComputerInfo>>> GetCustomerComputerInfoList(
+          Expression<Func<CustomerComputerInfo, bool>> filter = null,
+          Func<IQueryable<CustomerComputerInfo>, IOrderedQueryable<CustomerComputerInfo>> orderBy = null,
+          string includeProperties = "");
+        Task<OperationResponse<CustomerComputerInfo>> SaveCustomerComputerInfo(CustomerComputerInfo customer);
+        Task<OperationResponse<CustomerComputerInfo>> DeleteCustomerComputerInfoByCustomerComputerInfoId(int id);
+        Task<OperationResponse<CustomerComputerInfo>> GetByCustomerComputerInfoId(int id);
+        Task<OperationResponse<CustomerComputerInfo>> UpdateCustomerComputerInfo(CustomerComputerInfo model);
+        Task<OperationResponse<List<CustomerComputerInfo>>> GetByCustomerComputerInfoListBySubscriptionId(
+          int SubscriptionId);
+        Task<OperationResponse<int>> GetAlreadyComputerCountsBySubscriptionId(int subscriptionId);
+        Task<OperationResponse<CustomerComputerInfo>> GetByCustomerComputerHddAndMacAndProcessSerialCode(int subscriptionId, string hddCode,
+          string macCode, string processCode);
 
-  }
+    }
 }
